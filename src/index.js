965c8f0ef77134b0139ada12d5387248ac668f66
Khrystyna Kruk <krystynakruk0@gmail.com>
2024-04-07 13:55:00 +0200

    onClick

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
-
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
